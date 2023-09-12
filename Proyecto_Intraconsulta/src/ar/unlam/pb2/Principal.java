4614d8412ec70909a562580e15f123103cf59476
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2023-09-12 17:11:33 -0300

    editado en favio1

--- Proyecto_Intraconsulta/src/ar/unlam/pb2/Principal.java
+++ Proyecto_Intraconsulta/src/ar/unlam/pb2/Principal.java
@@ -3,7 +3,10 @@ package ar.unlam.pb2;
 public class Principal {
 
 	public static void main(String[] args) {
-		// favio
+		boolean soyPersona = true;
+		if (soyPersona) {
+			System.out.println("Nada");
+		}
 	}
 
 }

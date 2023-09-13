e490a35b12609063f5409772caea3f427c358f74
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2023-09-13 08:23:19 -0300

    gonza: avance en rama gonza

--- Proyecto_Intraconsulta/src/ar/unlam/pb2/Principal.java
+++ Proyecto_Intraconsulta/src/ar/unlam/pb2/Principal.java
@@ -3,7 +3,7 @@ package ar.unlam.pb2;
 public class Principal {
 
 	public static void main(String[] args) {
-		String cambio = "hecho desde rama gonza";
+		String cambio = "Gonza no trabaja";
 	}
 
 }

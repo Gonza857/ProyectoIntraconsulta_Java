722d90655e673f80228b350f7d44ec8cc875334b
Favio Gomez <favio.gz02@gmail.com>
2023-09-19 11:49:05 -0300

    master a favio

--- Proyecto_Intraconsulta/src/ar/unlam/pb2/Principal.java
+++ Proyecto_Intraconsulta/src/ar/unlam/pb2/Principal.java
@@ -5,7 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		String cambio = "Gonza no trabaja";
 
-		// hay que empezar a laburar flaco
+		// hay que empezar a laburar flaco hu lco
+		
 	}
 
 }

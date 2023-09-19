ea70fe685a61eadf9854057a8e0a74a273f799e5
Favio Gomez <favio.gz02@gmail.com>
2023-09-19 11:46:34 -0300

    oaoaa

--- Proyecto_Intraconsulta/src/ar/unlam/pb2/Principal.java
+++ Proyecto_Intraconsulta/src/ar/unlam/pb2/Principal.java
@@ -3,10 +3,5 @@ package ar.unlam.pb2;
 public class Principal {
 
 	public static void main(String[] args) {
-		boolean soyPersona = true;
-		if (soyPersona) {
-			System.out.println("Nada");
-		}
-	}
 
-}
+}}
